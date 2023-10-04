97826c54b489624e7a431241d8cfd27f7db866a4
jatnerubia <jlerit@nerubia.com>
2023-10-04 16:51:04 +0800

    added refresh token

---
+++ src/controllers/auth/refresh_token_controller.ts
@@ -0,0 +1,35 @@
+import { type Request, type Response } from "express"
+import jwt from "jsonwebtoken"
+
+interface DecodedToken {
+  email: string
+  firstName: string
+  lastName: string
+}
+
+export const refreshToken = async (req: Request, res: Response) => {
+  const cookies = req.cookies
+  if (cookies.jwt === undefined) {
+    return res.status(401).json({ message: "Unauthorized" })
+  }
+  jwt.verify(
+    cookies.jwt,
+    process.env.REFRESH_TOKEN_SECRET as string,
+    async (error: unknown, decoded: unknown) => {
+      if (error != null) return res.status(403).json({ message: "Forbidden" })
+      const decodedToken = decoded as DecodedToken
+      const accessToken = jwt.sign(
+        {
+          email: decodedToken.email,
+          firstName: decodedToken.firstName,
+          lastName: decodedToken.lastName,
+        },
+        process.env.ACCESS_TOKEN_SECRET as string,
+        {
+          expiresIn: "5m",
+        }
+      )
+      res.json({ accessToken })
+    }
+  )
+}
--- src/routes/auth_route.ts
+++ src/routes/auth_route.ts
@@ -1,8 +1,10 @@
 import express from "express"
 import { login } from "../controllers/auth/login_controller"
+import { refreshToken } from "../controllers/auth/refresh_token_controller"
 
 const router = express.Router()
 
 router.post("/login", login)
+router.get("/refresh", refreshToken)
 
 export default router
