155ece9dcdfb9878548520f93c42784b865a5a50
jatnerubia <jlerit@nerubia.com>
2023-10-12 13:19:19 +0800

    updated refresh token

--- src/controllers/auth/refresh_token_controller.ts
+++ src/controllers/auth/refresh_token_controller.ts
@@ -1,6 +1,7 @@
 import { type Request, type Response } from "express"
 import jwt from "jsonwebtoken"
 import { type UserToken } from "../../types/user_token"
+import prisma from "../../utils/prisma"
 
 export const refreshToken = async (req: Request, res: Response) => {
   const cookies = req.cookies
@@ -13,25 +14,43 @@ export const refreshToken = async (req: Request, res: Response) => {
     async (error: unknown, decoded: unknown) => {
       if (error != null) return res.status(403).json({ message: "Forbidden" })
       const decodedToken = decoded as UserToken
+
+      const existingUser = await prisma.users.findUnique({
+        where: {
+          email: decodedToken.email,
+        },
+      })
+
+      if (existingUser === null)
+        return res.status(403).json({ message: "Forbidden" })
+
       const accessToken = jwt.sign(
         {
-          id: decodedToken.id,
-          email: decodedToken.email,
-          firstName: decodedToken.firstName,
-          lastName: decodedToken.lastName,
+          id: existingUser.id,
+          email: existingUser.email,
+          firstName: existingUser.first_name,
+          lastName: existingUser.last_name,
         },
         process.env.ACCESS_TOKEN_SECRET as string,
         {
           expiresIn: process.env.ACCESS_TOKEN_EXPIRATION,
         }
       )
+
+      const userRoles = await prisma.user_roles.findMany({
+        where: {
+          user_id: existingUser.id,
+        },
+      })
+
       res.json({
         accessToken,
         user: {
-          id: decodedToken.id,
-          email: decodedToken.email,
-          firstName: decodedToken.firstName,
-          lastName: decodedToken.lastName,
+          id: existingUser.id,
+          email: existingUser.email,
+          firstName: existingUser.first_name,
+          lastName: existingUser.last_name,
+          roles: userRoles.map((role) => role.name),
         },
       })
     }
