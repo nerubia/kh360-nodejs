3f1fdba0c451fce2f9afbc82629cbc8d104d0b75
jatnerubia <jlerit@nerubia.com>
2023-10-20 12:57:19 +0800

    added fields in evaluations tables

---
+++ prisma/migrations/20231020040433_added_evaluations_relation_to_project_members/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `evaluations` ADD COLUMN `project_member_id` INTEGER NULL;
+
+-- AddForeignKey
+ALTER TABLE `evaluations` ADD CONSTRAINT `evaluations_project_member_id_fkey` FOREIGN KEY (`project_member_id`) REFERENCES `project_members`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
