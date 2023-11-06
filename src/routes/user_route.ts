74945f04dde15b40f2d186d25990ce6bb51db6c2
jatnerubia <jlerit@nerubia.com>
2023-11-06 10:42:25 +0800

    refactor: list user evaluations

--- src/routes/user_route.ts
+++ src/routes/user_route.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import { logout } from "../controllers/user/logout_controller"
 import {
+  getEvaluations,
   getProfile,
   sendSampleMail,
   submitAnswer,
@@ -8,6 +9,7 @@ import {
 
 const router = express.Router()
 
+router.get("/evaluations", getEvaluations)
 router.post("/evaluations/:id/submit-answer", submitAnswer)
 
 // TODO: Refactor
