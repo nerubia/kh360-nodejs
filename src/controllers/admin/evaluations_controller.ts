e9b7800cc3420a0e48f86e2bbd0d4c442dc34c7b
Emmalyn Acha <eacha@nerubia.com>
2023-11-06 11:34:11 +0800

    fix: add for_evaluation filter

--- src/controllers/admin/evaluations_controller.ts
+++ src/controllers/admin/evaluations_controller.ts
@@ -6,10 +6,23 @@ import { EvaluationStatus } from "../../types/evaluationType"
  * List evaluations based on provided filters.
  * @param req.query.evaluation_template_id - Filter by evaluation template id.
  * @param req.query.evaluation_result_id - Filter by evaluation result id.
+ * @param req.query.for_evaluation - Filter by for_evaluation.
  */
 export const index = async (req: Request, res: Response) => {
   try {
-    const { evaluation_template_id, evaluation_result_id } = req.query
+    const { evaluation_template_id, evaluation_result_id, for_evaluation } =
+      req.query
+
+    const where = {
+      evaluation_template_id: parseInt(evaluation_template_id as string),
+      evaluation_result_id: parseInt(evaluation_result_id as string),
+    }
+
+    if (for_evaluation !== undefined) {
+      Object.assign(where, {
+        for_evaluation: Boolean(for_evaluation),
+      })
+    }
 
     const evaluations = await prisma.evaluations.findMany({
       include: {
@@ -19,10 +32,7 @@ export const index = async (req: Request, res: Response) => {
           },
         },
       },
-      where: {
-        evaluation_template_id: parseInt(evaluation_template_id as string),
-        evaluation_result_id: parseInt(evaluation_result_id as string),
-      },
+      where,
       distinct: ["evaluator_id", "project_id"],
     })
 
