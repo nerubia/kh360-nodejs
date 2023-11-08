e0108d817e910aba1185e55e89db0eac74038806
jatnerubia <jlerit@nerubia.com>
2023-11-08 14:01:30 +0800

    added delete eval administration api

--- src/routes/admin/evaluation_administrations_route.ts
+++ src/routes/admin/evaluation_administrations_route.ts
@@ -6,6 +6,7 @@ import {
   update,
   generate,
   generateStatus,
+  destroy,
 } from "../../controllers/admin/evaluation_administrations_controller"
 
 const router = express.Router()
@@ -14,6 +15,8 @@ router.get("/", index)
 router.post("/", store)
 router.get("/:id", show)
 router.put("/:id", update)
+router.delete("/:id", destroy)
+
 router.get("/:id/generate-status", generateStatus)
 router.post("/:id/generate", generate)
 
