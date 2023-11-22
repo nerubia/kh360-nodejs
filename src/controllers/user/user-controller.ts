75442aaeddc8c57338f927497f819af8e0009a0e
jatnerubia <jlerit@nerubia.com>
2023-11-22 09:35:06 +0800

    added current page in response

--- src/controllers/user/user-controller.ts
+++ src/controllers/user/user-controller.ts
@@ -165,6 +165,7 @@ export const getEvaluationAdministrations = async (req: Request, res: Response) 
       pageInfo: {
         hasPreviousPage: currentPage > 1,
         hasNextPage: currentPage < totalPages,
+        currentPage,
         totalPages,
         totalItems,
       },
