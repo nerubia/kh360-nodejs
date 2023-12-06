dc300f9aba37c5f196fcea47462081b2a0f8d55c
Niño Allen Ardiente <ardientenino@gmail.com>
2023-12-06 14:15:17 +0800

    Update port handling

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import "./utils/scheduler"
 
 const PORT = process.env.PORT ?? 5000
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server running on port ${PORT}`)
 })
