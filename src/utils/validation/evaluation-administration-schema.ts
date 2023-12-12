06f93e3e468fd247604a2299d0f065af37a0ee15
Niño Allen Ardiente <ardientenino@gmail.com>
2023-12-12 16:56:10 +0800

    reverted evaluation-administration-schema.ts

--- src/utils/validation/evaluation-administration-schema.ts
+++ src/utils/validation/evaluation-administration-schema.ts
@@ -9,15 +9,6 @@ export const addEvaluatorSchema = object().shape({
   is_external: string().required("User is required"),
 })
 
-export const addEvaluatorSchema = object().shape({
-  evaluation_template_id: string().required("Template Type is required"),
-  evaluation_result_id: string().required("Evaluation result id is required"),
-  evaluee_id: string().required("Evaluee id is required"),
-  project_member_id: string().optional(),
-  user_id: string().required("User is required"),
-  is_external: string().required("User is required"),
-})
-
 export const addExternalEvaluatorsSchema = object().shape({
   evaluation_template_id: string().required("Evaluation template id is required"),
   evaluation_result_id: string().required("Evaluation result id is required"),
