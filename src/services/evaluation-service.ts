0efcac31f2f6a3c9e84a123e6d96daa1e939c685
jatnerubia <jlerit@nerubia.com>
2023-12-15 08:38:26 +0800

    updated approve api

--- src/services/evaluation-service.ts
+++ src/services/evaluation-service.ts
@@ -254,6 +254,16 @@ export const updateStatusByAdministrationId = async (
   await EvaluationRepository.updateStatusByAdministrationId(evaluation_administration_id, status)
 }
 
+export const deleteById = async (id: number) => {
+  const evaluation = await EvaluationRepository.getById(id)
+
+  if (evaluation === null) {
+    throw new CustomError("Id not found", 400)
+  }
+
+  await EvaluationRepository.deleteById(id)
+}
+
 export const countAllByFilters = async (where: Prisma.evaluationsWhereInput) => {
   return await EvaluationRepository.countAllByFilters(where)
 }
@@ -266,15 +276,16 @@ export const aggregateSumByFilters = async (
 }
 
 export const calculateZscore = async (evaluation_administration_id: number) => {
-  const uniqueEvaluations = await EvaluationRepository.getAllDistinctByFilters(
+  const uniqueInternalEvaluations = await EvaluationRepository.getAllDistinctByFilters(
     {
       evaluation_administration_id,
       status: EvaluationStatus.Submitted,
+      is_external: false,
     },
     ["evaluator_id"]
   )
 
-  for (const uniqueEvaluation of uniqueEvaluations) {
+  for (const uniqueEvaluation of uniqueInternalEvaluations) {
     const evaluations = await EvaluationRepository.getAllByFilters({
       evaluation_administration_id,
       evaluator_id: uniqueEvaluation.evaluator_id,
@@ -285,8 +296,49 @@ export const calculateZscore = async (evaluation_administration_id: number) => {
     const norms = await calculateNorms(scores)
 
     for (const evaluation of evaluations) {
-      const zscore = (Number(evaluation.score) - norms.mean) / norms.stdDev
-      const weighted_zscore = zscore * Number(evaluation.weight)
+      let zscore = 0
+      let weighted_zscore = 0
+
+      if (Number(evaluation.score) !== 0) {
+        zscore = (Number(evaluation.score) - norms.mean) / norms.stdDev
+        weighted_zscore = zscore * Number(evaluation.weight)
+      }
+
+      await EvaluationRepository.updateZScoreById(
+        evaluation.id,
+        isNaN(zscore) ? 0 : zscore,
+        isNaN(weighted_zscore) ? 0 : weighted_zscore
+      )
+    }
+  }
+
+  const uniqueExternalEvaluations = await EvaluationRepository.getAllDistinctByFilters(
+    {
+      evaluation_administration_id,
+      status: EvaluationStatus.Submitted,
+      is_external: true,
+    },
+    ["external_evaluator_id"]
+  )
+
+  for (const uniqueEvaluation of uniqueExternalEvaluations) {
+    const evaluations = await EvaluationRepository.getAllByFilters({
+      evaluation_administration_id,
+      external_evaluator_id: uniqueEvaluation.external_evaluator_id,
+      status: EvaluationStatus.Submitted,
+    })
+
+    const scores = evaluations.map((evaluation) => Number(evaluation.score))
+    const norms = await calculateNorms(scores)
+
+    for (const evaluation of evaluations) {
+      let zscore = 0
+      let weighted_zscore = 0
+
+      if (Number(evaluation.score) !== 0) {
+        zscore = (Number(evaluation.score) - norms.mean) / norms.stdDev
+        weighted_zscore = zscore * Number(evaluation.weight)
+      }
 
       await EvaluationRepository.updateZScoreById(
         evaluation.id,
