57d302156f18dadda58219fc27953ba453f0b870
jatnerubia <jlerit@nerubia.com>
2023-12-18 14:23:12 +0800

    added compute score rating

--- src/repositories/score-rating-repository.ts
+++ src/repositories/score-rating-repository.ts
@@ -1,21 +1,6 @@
 import { type Decimal } from "@prisma/client/runtime/library"
 import prisma from "../utils/prisma"
 
-export const getById = async (id: number) => {
-  return await prisma.score_ratings.findUnique({
-    select: {
-      id: true,
-      name: true,
-      display_name: true,
-      evaluee_description: true,
-      result_description: true,
-    },
-    where: {
-      id,
-    },
-  })
-}
-
 export const getByScore = async (score: Decimal) => {
   return await prisma.score_ratings.findFirst({
     where: {
@@ -27,8 +12,4 @@ export const getByScore = async (score: Decimal) => {
       },
     },
   })
-}
-
-export const getScoreRatings = async () => {
-  return await prisma.score_ratings.findMany()
 }
