0e1cabfd03a54fb65ea6d0f7e994dc22fe313058
jatnerubia <jlerit@nerubia.com>
2023-12-18 09:55:10 +0800

    updated project member api

--- src/utils/validation/project-member-schema.ts
+++ src/utils/validation/project-member-schema.ts
@@ -1,9 +1,14 @@
-export interface ProjectMember {
-  project_id?: number
-  user_id?: number
-  project_role_id?: number
-  start_date?: Date | string
-  end_date?: Date | string
-  allocation_rate?: number
-  remarks?: string
-}
+import { number, object, string } from "yup"
+
+export const createProjectMemberSchema = object().shape({
+  project_id: number().required("Project id is required"),
+  user_id: number().required("User id is required"),
+  project_role_id: number().required("Project role id is required"),
+  start_date: string().required("Start date is required"),
+  end_date: string().required("End date is required"),
+  allocation_rate: number()
+    .required("Allocation rate is required")
+    .test("is-valid-allocation-rate", "Allocation rate must be between 0 and 100", (value) => {
+      return typeof value === "number" && value >= 0 && value <= 100
+    }),
+})
