149ecc7e0abf763fe817f179eade91b7b2f55a4f
jatnerubia <jlerit@nerubia.com>
2023-12-19 16:28:56 +0800

    updated get evaluators to include email logs

--- src/repositories/email-log-repository.ts
+++ src/repositories/email-log-repository.ts
@@ -1,11 +1,4 @@
-import { type EmailLog } from "../types/email-log-type"
 import prisma from "../utils/prisma"
-
-export const create = async (data: EmailLog) => {
-  return await prisma.email_logs.create({
-    data,
-  })
-}
 
 export const getAllByEmail = async (email_address: string) => {
   return await prisma.email_logs.findMany({
