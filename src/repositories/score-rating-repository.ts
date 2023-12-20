8b6424dd35a854bd9355ea464f6b64b9eaf10529
jatnerubia <jlerit@nerubia.com>
2023-12-20 10:51:54 +0800

    updated get user eval result

--- src/repositories/score-rating-repository.ts
+++ src/repositories/score-rating-repository.ts
@@ -1,6 +1,18 @@
 import { type Decimal } from "@prisma/client/runtime/library"
 import prisma from "../utils/prisma"
 
+export const getById = async (id: number) => {
+  return await prisma.score_ratings.findUnique({
+    select: {
+      display_name: true,
+      evaluee_description: true,
+    },
+    where: {
+      id,
+    },
+  })
+}
+
 export const getByScore = async (score: Decimal) => {
   return await prisma.score_ratings.findFirst({
     where: {
