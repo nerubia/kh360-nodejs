e16f621c9b6d3430077104ca40d8438a91937c4e
jatnerubia <jlerit@nerubia.com>
2023-12-20 14:57:56 +0800

    added user settings in auth response

--- src/utils/sendgrid.ts
+++ src/utils/sendgrid.ts
@@ -11,7 +11,9 @@ export const sendMail = async (to: string, subject: string, content: string) => 
       html: content,
     }
     return await sgMail.send(msg)
-  } catch (error) {}
+  } catch (error) {
+    return null
+  }
 }
 
 export const sendMultipleMail = async (to: string[], subject: string, content: string) => {
