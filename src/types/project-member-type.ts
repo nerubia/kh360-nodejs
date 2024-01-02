20dccde5055e96461977a26b05f7a60763179dc8
emmaacha <emmalyncayuna@gmail.com>
2024-01-02 18:49:03 +0800

    fix error

--- src/types/project-member-type.ts
+++ src/types/project-member-type.ts
@@ -1,38 +1,8 @@
-import { type Prisma } from "@prisma/client"
-import prisma from "../utils/prisma"
-
-export const getById = async (id: number) => {
-  return await prisma.projects.findUnique({
-    select: {
-      id: true,
-      name: true,
-    },
-    where: {
-      id,
-    },
-  })
-}
-
-export const getAllByName = async (name: string) => {
-  return await prisma.projects.findMany({
-    select: {
-      id: true,
-      name: true,
-    },
-    where: {
-      name: {
-        contains: name,
-      },
-    },
-  })
-}
-
-export const getAllByFilters = async (where: Prisma.projectsWhereInput) => {
-  return await prisma.projects.findMany({
-    select: {
-      id: true,
-      name: true,
-    },
-    where,
-  })
+export interface ProjectMember {
+  project_id?: number
+  user_id?: number
+  project_role_id?: number
+  start_date?: Date | string
+  end_date?: Date | string
+  allocation_rate?: number
 }
