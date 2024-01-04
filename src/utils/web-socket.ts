8859469a0b567df3b659acb292ff449ad04875b2
Niño Allen Ardiente <ardientenino@gmail.com>
2024-01-04 13:31:48 +0800

    implemented websocket

---
+++ src/utils/web-socket.ts
@@ -0,0 +1,34 @@
+import WebSocket from "ws"
+import logger from "./logger"
+import { type Application } from "express"
+import http from "http"
+
+const webSocketServer = (app: Application) => {
+  const server = http.createServer(app)
+  const wss = new WebSocket.Server({ path: "/ws", server })
+
+  wss.on("connection", (ws: WebSocket) => {
+    logger.info("WebSocket connected")
+
+    // Handle incoming messages
+    ws.on("message", (message: string) => {
+      logger.info(`Received: ${message}`)
+
+      // Broadcast the message to all connected clients
+      wss.clients.forEach((client) => {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(message.toString())
+        }
+      })
+    })
+
+    // Handle WebSocket closing
+    ws.on("close", () => {
+      logger.info("WebSocket disconnected")
+    })
+  })
+
+  return server
+}
+
+export default webSocketServer
