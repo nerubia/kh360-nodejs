56457d8c4b27deb6b59219c0efc06c164a9218c9
Niño Allen Ardiente <ardientenino@gmail.com>
2024-01-04 13:31:48 +0800

    implemented websocket

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,15 @@ import "dotenv/config"
 import app from "./app"
 import "./utils/scheduler"
 import logger from "./utils/logger"
+import webSocketServer from "./utils/web-socket"
 
 const PORT = process.env.PORT ?? 5000
 
-app.listen(PORT, () => {
+/**
+ * WebSocket
+ */
+const server = webSocketServer(app)
+
+server.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`)
 })
