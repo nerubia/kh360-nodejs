25d1ce68eaa539555f7596bf78d4cbfabf2b6abb
jatnerubia <jlerit@nerubia.com>
2024-01-09 14:11:58 +0800

    updated get projects api

--- src/routes/admin/project-route.ts
+++ src/routes/admin/project-route.ts
@@ -3,6 +3,7 @@ import * as ProjectController from "../../controllers/admin/project-controller"
 
 const router = express.Router()
 
+router.get("/all", ProjectController.all)
 router.get("/", ProjectController.index)
 
 export default router
