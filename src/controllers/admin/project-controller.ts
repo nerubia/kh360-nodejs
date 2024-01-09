23778e7dd0cb6c9be10904c9b4a7fd09983b071f
jatnerubia <jlerit@nerubia.com>
2024-01-09 14:11:58 +0800

    updated get projects api

--- src/controllers/admin/project-controller.ts
+++ src/controllers/admin/project-controller.ts
@@ -2,13 +2,37 @@ import { type Request, type Response } from "express"
 import * as ProjectService from "../../services/project-service"
 
 /**
- * List projects based on provided filters.
+ * List all projects based on provided filters.
  * @param req.query.name - Filter by name.
  */
-export const index = async (req: Request, res: Response) => {
+export const all = async (req: Request, res: Response) => {
   try {
     const { name } = req.query
     const results = await ProjectService.getAllByFilters(name as string)
+    res.json(results)
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" })
+  }
+}
+
+/**
+ * List projects based on provided filters.
+ * @param req.query.name - Filter by name.
+ * @param req.query.client - Filter by client.
+ * @param req.query.skills - Filter by skills.
+ * @param req.query.status - Filter by status.
+ * @param req.query.page - Filter by page.
+ */
+export const index = async (req: Request, res: Response) => {
+  try {
+    const { name, client, skills, status, page } = req.query
+    const results = await ProjectService.paginateByFilters(
+      name as string,
+      client as string,
+      skills as string,
+      status as string,
+      page as string
+    )
     res.json(results)
   } catch (error) {
     res.status(500).json({ message: "Something went wrong" })
--- src/services/project-service.ts
+++ src/services/project-service.ts
@@ -1,4 +1,7 @@
+import { type Prisma } from "@prisma/client"
+import * as ClienRepository from "../repositories/client-repository"
 import * as ProjectRepository from "../repositories/project-repository"
+import * as ProjectSkillRepository from "../repositories/project-skill-repository"
 
 export const getById = async (id: number) => {
   return await ProjectRepository.getById(id)
@@ -11,4 +14,73 @@ export const getAllByFilters = async (name: string) => {
     },
   }
   return await ProjectRepository.getAllByFilters(where)
+}
+
+export const paginateByFilters = async (
+  name?: string,
+  client?: string,
+  skills?: string,
+  status?: string,
+  page?: string
+) => {
+  const itemsPerPage = 20
+  const parsedPage = parseInt(page as string)
+  const currentPage = isNaN(parsedPage) || parsedPage < 0 ? 1 : parsedPage
+
+  const where: Prisma.projectsWhereInput = {}
+
+  if (name !== undefined) {
+    Object.assign(where, {
+      name: {
+        contains: name,
+      },
+    })
+  }
+
+  if (client !== undefined) {
+    const clients = await ClienRepository.getAllByName(client)
+    const clientids = clients.map((c) => c.id)
+    Object.assign(where, {
+      client_id: {
+        in: clientids,
+      },
+    })
+  }
+
+  if (skills !== undefined) {
+    const projectSkills = await ProjectSkillRepository.getAllBySkillName(skills)
+    const projectIds = projectSkills.map((projectSkill) => projectSkill.project_id)
+    Object.assign(where, {
+      id: {
+        in: projectIds,
+      },
+    })
+  }
+
+  if (status !== undefined && status !== "all") {
+    Object.assign(where, {
+      status,
+    })
+  }
+
+  const totalItems = await ProjectRepository.countByFilters(where)
+  const totalPages = Math.ceil(totalItems / itemsPerPage)
+
+  const projects = await ProjectRepository.paginateByFilters(
+    (currentPage - 1) * itemsPerPage,
+    itemsPerPage,
+    where
+  )
+
+  const pageInfo = {
+    hasPreviousPage: currentPage > 1,
+    hasNextPage: currentPage < totalPages,
+    totalPages,
+    totalItems,
+  }
+
+  return {
+    data: projects,
+    pageInfo,
+  }
 }
