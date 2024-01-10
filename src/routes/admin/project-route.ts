6257b30b571d876313959235e9c848ebab8cd6aa
jatnerubia <jlerit@nerubia.com>
2024-01-10 14:11:18 +0800

    added get, create, update project api

--- src/routes/admin/project-route.ts
+++ src/routes/admin/project-route.ts
@@ -7,6 +7,9 @@ router.get("/all", ProjectController.all)
 router.get("/status", ProjectController.getAllStatus)
 
 router.get("/", ProjectController.index)
+router.post("/", ProjectController.store)
+router.get("/:id", ProjectController.show)
+router.put("/:id", ProjectController.update)
 router.delete("/:id", ProjectController.destroy)
 
 export default router
