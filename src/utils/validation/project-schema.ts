885fcd7b38c054fb1147a259b9879bbb1420b024
emmaacha <emmalynacha@gmail.com>
2024-02-13 16:25:53 +0800

    update apis for projects form enhancements

--- src/utils/validation/project-schema.ts
+++ src/utils/validation/project-schema.ts
@@ -3,13 +3,15 @@ import { number, object, string } from "yup"
 export const createProjectSchema = object().shape({
   name: string().required("Name is required"),
   client_id: number().optional(),
-  start_date: string()
-    .required("Start date is required")
-    .test("start_date", "Start date must be before end date", function (start_date) {
+  start_date: string().test(
+    "start_date",
+    "Start date must be before end date",
+    function (start_date) {
       const end_date = this.parent.end_date
-      return new Date(start_date) <= new Date(end_date)
-    }),
-  end_date: string().required("End date is required"),
+      return new Date(start_date ?? "") <= new Date(end_date)
+    }
+  ),
+  end_date: string(),
   description: string().required("Description is required"),
   status: string().required("Status is required"),
 })
