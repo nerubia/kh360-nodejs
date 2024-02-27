6efff81e5966701477e256a8a338ec4fbe0929f1
emmaacha <emmalynacha@gmail.com>
2024-02-27 11:36:19 +0800

    auto update status when it changes via scheduler

--- src/jobs/send-evaluation-email-job.ts
+++ src/jobs/send-evaluation-email-job.ts
@@ -1,5 +1,12 @@
 import * as EvaluationAdministrationService from "../services/evaluation-administration-service"
 import { EvaluationAdministrationStatus } from "../types/evaluation-administration-type"
+import WebSocket from "ws"
+
+let wssInstance: WebSocket.Server | null = null
+
+export const setWssForProcessingEvalAdmin = (wss: WebSocket.Server) => {
+  wssInstance = wss
+}
 
 export const sendEvaluationEmailJob = async () => {
   const evaluationAdministrations = await EvaluationAdministrationService.getAllByStatus(
@@ -11,6 +18,19 @@ export const sendEvaluationEmailJob = async () => {
       EvaluationAdministrationStatus.Ongoing
     )
 
-    // await EvaluationAdministrationService.sendEvaluationEmailById(evaluationAdministration.id)
+    await EvaluationAdministrationService.sendEvaluationEmailById(evaluationAdministration.id)
+
+    if (wssInstance !== null) {
+      const message = JSON.stringify({
+        event: "setEvaluationAdministrationToOngoing",
+        data: "setEvaluationAdministrationToOngoing",
+      })
+
+      wssInstance.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(message)
+        }
+      })
+    }
   }
 }
