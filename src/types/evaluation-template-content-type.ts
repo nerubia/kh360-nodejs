40012324bc838c2acc63f6b1d0bd414409ce6c2b
emmaacha <emmalynacha@gmail.com>
2024-03-05 16:03:48 +0800

    include template content when in update eval template api

---
+++ src/types/evaluation-template-content-type.ts
@@ -0,0 +1,12 @@
+export interface EvaluationTemplateContent {
+  id: number
+  evaluation_template_id: number
+  name?: string
+  description?: string
+  answerId?: number
+  average_rate?: number
+  category?: string
+  rate?: string
+  is_active?: boolean
+  sequence_no?: number
+}
