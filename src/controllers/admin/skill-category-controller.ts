c8349f5741a750e6128cce39ab098f1b25aa3316
Chadie Gil <caugis@nerubia.com>
2024-03-11 14:36:36 +0800

    node-499-create-crud-api-skills-category

--- src/controllers/admin/skill-category-controller.ts
+++ src/controllers/admin/skill-category-controller.ts
@@ -1,5 +1,8 @@
 import { type Request, type Response } from "express"
 import * as SkillCategoryService from "../../services/skill-category-service"
+import { createSkillCategorySchema } from "../../utils/validation/skill-category-schema"
+import { ValidationError } from "yup"
+import CustomError from "../../utils/custom-error"
 
 /**
  * List all skill categories
@@ -9,6 +12,92 @@ export const index = async (req: Request, res: Response) => {
     const skill_categories = await SkillCategoryService.getAllSkillCategories()
     res.json(skill_categories)
   } catch (error) {
+    res.status(500).json({ message: "Something went wrong" })
+  }
+}
+/**
+ * Show details of a single skill category
+ * @param req.params.id - The ID of the skill category to retrive
+ */
+export const show = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+    const skillCategory = await SkillCategoryService.getById(parseInt(id))
+    res.json(skillCategory)
+  } catch (error) {
+    if (error instanceof ValidationError) {
+      return res.status(400).json(error)
+    }
+    if (error instanceof CustomError) {
+      return res.status(error.status).json({ message: error.message })
+    }
+    res.status(500).json({ message: "Something went wrong" })
+  }
+}
+
+/**
+ * Store new skill category
+ * @param req.body.name - The name of the skill category
+ * @param req.body.description - The description of the skill category
+ * @param req.body.status - The status of the skill category
+ */
+export const store = async (req: Request, res: Response) => {
+  try {
+    const { name, description, status } = req.body
+    await createSkillCategorySchema.validate({ name, description, status })
+    const newSkillCategory = await SkillCategoryService.create({
+      name,
+      description,
+      status,
+    })
+    res.json(newSkillCategory)
+  } catch (error) {
+    res.status(500).json({ message: error })
+  }
+}
+/**
+ * Update skill category
+ * @param req.body.name - The name of the skill category
+ * @param req.body.description - The description of the skill category
+ * @param req.body.status - The status of the skill category
+ */
+export const update = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+    const { name, description, status } = req.body
+    await createSkillCategorySchema.validate({ name, description, status })
+    const updateSkillCategory = await SkillCategoryService.updatedById(parseInt(id), {
+      name,
+      description,
+      status,
+    })
+    res.json(updateSkillCategory)
+  } catch (error) {
+    if (error instanceof ValidationError) {
+      return res.status(400).json(error)
+    }
+    if (error instanceof CustomError) {
+      return res.status(error.status).json({ message: error.message })
+    }
+    res.status(500).json({ message: "Something went wrong" })
+  }
+}
+/**
+ * Delete skill category
+ * @param req.params.id - The ID of the skill category to be deleted
+ */
+export const destroy = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+    await SkillCategoryService.deletedById(parseInt(id))
+    res.json({ id, message: "Skill category successfully deleted" })
+  } catch (error) {
+    if (error instanceof ValidationError) {
+      return res.status(400).json(error)
+    }
+    if (error instanceof CustomError) {
+      return res.status(error.status).json({ message: error.message })
+    }
     res.status(500).json({ message: "Something went wrong" })
   }
 }
--- src/services/skill-category-service.ts
+++ src/services/skill-category-service.ts
@@ -1,7 +1,70 @@
 import * as SkillCategoryRepository from "../repositories/skill-category-repository"
-
+import { type SkillCategory } from "../types/skill-category-type"
+import CustomError from "../utils/custom-error"
+import * as SkilRepository from "../repositories/skill-repository"
 export const getAllSkillCategories = async () => {
   return await SkillCategoryRepository.getAllByFilters({
     status: true,
   })
+}
+
+export const create = async (data: SkillCategory) => {
+  const skillCategoryName = await SkillCategoryRepository.getByName(data.name)
+  if (skillCategoryName != null) {
+    throw new CustomError("Skill Category name should be unique", 400)
+  }
+  const findSkillcategory = await SkillCategoryRepository.findSkillCategoryDesc()
+  const nextSequenceNo =
+    findSkillcategory?.sequence_no !== null && findSkillcategory?.sequence_no !== undefined
+      ? findSkillcategory.sequence_no + 1
+      : 1
+  return await SkillCategoryRepository.create(data, nextSequenceNo)
+}
+
+export const updatedById = async (id: number, data: SkillCategory) => {
+  const skillCategory = await SkillCategoryRepository.getById(id)
+  const skillCategoryName = await SkillCategoryRepository.getByName(data.name)
+  if (skillCategoryName != null) {
+    throw new CustomError("Skill Category name should be unique", 400)
+  }
+  if (skillCategory === null) {
+    throw new CustomError("Skill Category not found", 400)
+  }
+  return await SkillCategoryRepository.update(id, data)
+}
+
+export const deletedById = async (id: number) => {
+  const skillCategory = await SkillCategoryRepository.getById(id)
+  const skillCategoryInUsed = await SkilRepository.getByFilters({
+    skill_category_id: id,
+  })
+  if (skillCategoryInUsed != null) {
+    throw new CustomError("Cannot delete skill category that is used in skills table", 400)
+  }
+  if (skillCategory === null) {
+    throw new CustomError("Skill Category not found", 400)
+  }
+  await SkillCategoryRepository.destroy(id, skillCategory.id)
+
+  const remainingSkillCategories = await SkillCategoryRepository.getAllByFilters({})
+  for (const category of remainingSkillCategories) {
+    if (category.sequence_no !== null && category.sequence_no !== undefined) {
+      const newData = {
+        sequence_no: remainingSkillCategories.indexOf(category) + 1,
+      }
+      await SkillCategoryRepository.update(category.id, newData)
+    }
+  }
+}
+
+export const getById = async (id: number) => {
+  const skillCategory = await SkillCategoryRepository.getById(id)
+  if (skillCategory === null) {
+    throw new CustomError("Skill Category not found", 400)
+  }
+  return await SkillCategoryRepository.show(id)
+}
+
+export const nextSequenceNo = async (data: SkillCategory) => {
+  return data?.sequence_no !== null && data?.sequence_no !== undefined ? data.sequence_no + 1 : 1
 }
