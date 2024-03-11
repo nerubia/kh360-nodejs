94e35013585754c638d2ff6115bc7fe012b0d7da
emmaacha <emmalynacha@gmail.com>
2024-03-11 15:41:42 +0800

    update batch process schedules

--- src/utils/scheduler.ts
+++ src/utils/scheduler.ts
@@ -6,17 +6,15 @@ import { sendEvaluationEmailJob } from "../jobs/send-evaluation-email-job"
 import { sendSurveyEmailJob } from "../jobs/send-survey-email-job"
 import { sendSkillMapEmailJob } from "../jobs/send-skill-map-email-job"
 
-// 12am UTC - (8am SGT) 0 0 * * *
-// (5 mins for now)
-const everyDay = schedule.scheduleJob("*/1 * * * *", async () => {
+// 12am UTC - (8am SGT)
+const everyDay = schedule.scheduleJob("0 0 * * *", async () => {
   await updateEvaluationAdministrationsJob()
   await updateSurveyAdministrationsJob()
   await updateSkillMapAdministrationsJob()
 })
 
 // every hour 0 * * * *
-// (5mins for now)
-const everyHour = schedule.scheduleJob("*/1 * * * *", async () => {
+const everyHour = schedule.scheduleJob("0 * * * *", async () => {
   await sendEvaluationEmailJob()
   await sendSurveyEmailJob()
   await sendSkillMapEmailJob()
