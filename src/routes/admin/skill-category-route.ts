9a706d2878a93b4c13fefdd8be3a516e8fc2f918
emmaacha <emmalynacha@gmail.com>
2024-03-12 17:40:37 +0800

    update api to sort by sequence numbers

--- src/routes/admin/skill-category-route.ts
+++ src/routes/admin/skill-category-route.ts
@@ -4,10 +4,12 @@ import * as SkillCategoryController from "../../controllers/admin/skill-category
 const router = express.Router()
 
 router.get("/all", SkillCategoryController.getAll)
+router.put("/sort", SkillCategoryController.updateSequenceNumbers)
 
 router.get("/", SkillCategoryController.index)
 router.post("/", SkillCategoryController.store)
 router.put("/:id", SkillCategoryController.update)
 router.delete("/:id", SkillCategoryController.destroy)
 router.get("/:id", SkillCategoryController.show)
+
 export default router
