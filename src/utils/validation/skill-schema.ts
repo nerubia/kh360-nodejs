249027506332f2b3e0e71f5748ff8105e05389d7
emmaacha <emmalynacha@gmail.com>
2024-03-13 18:26:18 +0800

    update apis for skills crud operations

--- src/utils/validation/skill-schema.ts
+++ src/utils/validation/skill-schema.ts
@@ -1,8 +1,8 @@
-import { number, object, string } from "yup"
+import { number, object, string, boolean } from "yup"
 
 export const createSkillSchema = object().shape({
   name: string().required("Skill name is required"),
   description: string(),
-  status: string().required("Status is required"),
+  status: boolean().required("Status is required"),
   skill_category_id: number().required("Skill category id is required"),
 })
