06b25fd0c1b151d22b816341f64b1cf42740026b
emmaacha <emmalynacha@gmail.com>
2024-03-27 19:34:17 +0800

    update apis to get survey results for view survey results screen

--- src/repositories/survey-template-answer-repository.ts
+++ src/repositories/survey-template-answer-repository.ts
@@ -4,6 +4,9 @@ import { type Prisma } from "@prisma/client"
 export const getAllByFilters = async (where: Prisma.survey_template_answersWhereInput) => {
   return await prisma.survey_template_answers.findMany({
     where,
+    orderBy: {
+      sequence_no: "asc",
+    },
   })
 }
 
