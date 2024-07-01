b6c4e964fdd930c6fa1adccfeecdcb735812df17
jatnerubia <jlerit@nerubia.com>
2024-07-01 13:46:40 +0800

    update: get user rating api

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -959,6 +959,11 @@ export const getSkillMapRatings = async (skill_map_administration_id: number, us
     user_skill_map_ratings: userSkillMapRatings,
     skill_map_administration: skillMapAdministration,
     skill_map_result_status: skillMapResult.status,
+    skill_map_result: {
+      submitted_date: skillMapResult.submitted_date,
+      comments: skillMapResult.comments,
+      status: skillMapResult.status,
+    },
     comments: skillMapResult.comments,
   }
 }
