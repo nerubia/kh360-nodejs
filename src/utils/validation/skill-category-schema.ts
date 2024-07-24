b133ad043b9910b64dacc3bec7216833ce608ded
Chadie Gil <caugis@nerubia.com>
2024-07-24 14:55:40 +0800

    node-1372-edit-skill-category-description-not-required

--- src/utils/validation/skill-category-schema.ts
+++ src/utils/validation/skill-category-schema.ts
@@ -2,6 +2,6 @@ import { boolean, object, string } from "yup"
 
 export const createSkillCategorySchema = object().shape({
   name: string().required("Skills Category name is required"),
-  description: string(),
+  description: string().nullable(),
   status: boolean().required("Status is required"),
 })
