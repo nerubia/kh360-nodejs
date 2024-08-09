e6cb06912fb3aa910d52599644ec3d91c316faa6
jatnerubia <jlerit@nerubia.com>
2024-08-09 10:21:24 +0800

    update: create batch api

--- src/types/test-batch-type.ts
+++ src/types/test-batch-type.ts
@@ -2,4 +2,5 @@ export interface TestBatch {
   name: string
   description: string
   status: boolean
+  itemIds: number[]
 }
--- src/utils/validation/test-batch-schema.ts
+++ src/utils/validation/test-batch-schema.ts
@@ -1,7 +1,8 @@
-import { boolean, object, string } from "yup"
+import { array, boolean, number, object, string } from "yup"
 
 export const createTestBatchSchema = object().shape({
   name: string().required("Name is required"),
   description: string().required("Description is required"),
   status: boolean().required("Status is required"),
+  itemIds: array().of(number()),
 })
