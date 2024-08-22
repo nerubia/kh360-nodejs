023534dd3e3c772ed9db4531a8fba0858ef89620
Chadie Gil <caugis@gmail.com>
2024-08-22 13:25:05 +0800

    update: Description for test test-item test-batch not required

--- src/utils/validation/test-item-schema.ts
+++ src/utils/validation/test-item-schema.ts
@@ -4,6 +4,5 @@ export const createTestItemSchema = object().shape({
   apiId: number().required("Api ID is required"),
   payload: string().required("Payload is required"),
   response: string().required("Response is required"),
-  description: string().required("Description is required"),
   status: boolean().required("Status is required"),
 })
