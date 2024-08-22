99b56d97f16d8608fffefade2e4761b89954312b
Chadie Gil <caugis@gmail.com>
2024-08-22 13:25:05 +0800

    update: Description for test test-item test-batch not required

--- src/utils/validation/test-batch-schema.ts
+++ src/utils/validation/test-batch-schema.ts
@@ -3,7 +3,6 @@ import { array, boolean, number, object, string } from "yup"
 export const createTestBatchSchema = object().shape({
   apiId: number().required("Api ID is required"),
   name: string().required("Name is required"),
-  description: string().required("Description is required"),
   status: boolean().required("Status is required"),
   itemIds: array().of(number()),
 })
