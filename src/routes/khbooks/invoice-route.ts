11db9a9cbc45bdc99388aa7526b0be3a498c0c50
jatnerubia <jlerit@nerubia.com>
2024-08-27 09:58:13 +0800

    feat: generate invoice pdf

---
+++ src/routes/khbooks/invoice-route.ts
@@ -0,0 +1,14 @@
+import express from "express"
+import * as InvoiceController from "../../controllers/khbooks/invoice-controller"
+
+const router = express.Router()
+
+// router.get("/", InvoiceController.index)
+// router.post("/", InvoiceController.store)
+// router.get("/:id", InvoiceController.show)
+// router.put("/:id", InvoiceController.update)
+// router.delete("/:id", InvoiceController.destroy)
+
+router.post("/:id/send", InvoiceController.sendInvoice)
+
+export default router
