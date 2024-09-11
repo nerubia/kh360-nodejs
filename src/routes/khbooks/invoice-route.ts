9553c331004834f065ca3411562ee36fc5297831
jatnerubia <jlerit@nerubia.com>
2024-09-11 09:33:28 +0800

    update: add invoice api endpoints: delete, send reminder, duplicate, download, cancel

--- src/routes/khbooks/invoice-route.ts
+++ src/routes/khbooks/invoice-route.ts
@@ -7,8 +7,12 @@ router.get("/", InvoiceController.index)
 router.post("/", InvoiceController.store)
 // router.get("/:id", InvoiceController.show)
 // router.put("/:id", InvoiceController.update)
-// router.delete("/:id", InvoiceController.destroy)
+router.delete("/:id", InvoiceController.destroy)
 
-router.post("/:id/send", InvoiceController.sendInvoice)
+router.post("/:id/send", InvoiceController.send)
+router.post("/:id/send-reminder", InvoiceController.sendReminder)
+router.post("/:id/duplicate", InvoiceController.duplicate)
+router.post("/:id/download", InvoiceController.download)
+router.post("/:id/cancel", InvoiceController.cancel)
 
 export default router
