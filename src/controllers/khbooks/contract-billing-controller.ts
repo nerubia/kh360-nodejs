48c50159ee6e1b0de5b79669d1c0741dce8e0981
jatnerubia <jasonlerit077@gmail.com>
2024-09-17 10:58:42 +0800

    update: contract billing api

--- src/controllers/khbooks/contract-billing-controller.ts
+++ src/controllers/khbooks/contract-billing-controller.ts
@@ -4,6 +4,7 @@ import * as ContractBillingService from "../../services/khbooks/contract-billing
 
 /**
  * List contract billings based on provided filters.
+ * @param req.query.client_id - Filter by client_id.
  * @param req.query.start_date - Filter by start_date.
  * @param req.query.end_date - Filter by end_date.
  * @param req.query.contract_no - Filter by contract_no.
@@ -14,8 +15,10 @@ import * as ContractBillingService from "../../services/khbooks/contract-billing
  */
 export const index = async (req: Request, res: Response) => {
   try {
-    const { start_date, end_date, contract_no, project_id, description, status, page } = req.query
+    const { client_id, start_date, end_date, contract_no, project_id, description, status, page } =
+      req.query
     const results = await ContractBillingService.getAllByFilters(
+      parseInt(client_id as string),
       start_date as string,
       end_date as string,
       contract_no as string,
