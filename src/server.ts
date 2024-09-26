e8a869962daf1a3b751f16399146fe54111357a9
jatnerubia <jasonlerit077@gmail.com>
2024-09-26 10:33:15 +0800

    feat: redis

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import app from "./app"
 import "./utils/scheduler"
 import logger from "./utils/logger"
 import webSocketServer from "./utils/web-socket"
+import { connectRedis } from "./utils/redis"
 
 const PORT = process.env.PORT ?? 5000
 
@@ -10,6 +11,8 @@ const PORT = process.env.PORT ?? 5000
  * WebSocket
  */
 const server = webSocketServer(app)
+
+void connectRedis()
 
 server.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`)
