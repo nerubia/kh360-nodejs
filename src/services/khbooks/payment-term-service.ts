70c2f7aa461378fd5dfde5b1e5b81361e92f08f8
jatnerubia <jasonlerit077@gmail.com>
2024-09-26 15:42:46 +0800

    update: country, currency, payment account, term, tax type and oum services

--- src/services/khbooks/payment-term-service.ts
+++ src/services/khbooks/payment-term-service.ts
@@ -1,38 +1,49 @@
 import { type Prisma } from "@prisma/client"
 import * as PaymentTermRepository from "../../repositories/khbooks/payment-term-repository"
+import { getCache, setCache } from "../../utils/redis"
 
 export const getAllByFilters = async (name: string, page: string) => {
-  const itemsPerPage = 20
-  const parsedPage = parseInt(page)
-  const currentPage = isNaN(parsedPage) || parsedPage < 0 ? 1 : parsedPage
+  const KEY = `PAYMENT_TERMS_${name}_${page}`
 
-  const where: Prisma.payment_termsWhereInput = {}
+  let results = await getCache(KEY)
 
-  if (name !== undefined) {
-    Object.assign(where, {
-      name: {
-        contains: name,
+  if (results === null) {
+    const itemsPerPage = 20
+    const parsedPage = parseInt(page)
+    const currentPage = isNaN(parsedPage) || parsedPage < 0 ? 1 : parsedPage
+
+    const where: Prisma.payment_termsWhereInput = {}
+
+    if (name !== undefined) {
+      Object.assign(where, {
+        name: {
+          contains: name,
+        },
+      })
+    }
+
+    const paymentTerms = await PaymentTermRepository.paginateByFilters(
+      (currentPage - 1) * itemsPerPage,
+      itemsPerPage,
+      where
+    )
+
+    const totalItems = await PaymentTermRepository.countAllByFilters(where)
+    const totalPages = Math.ceil(totalItems / itemsPerPage)
+
+    results = {
+      data: paymentTerms,
+      pageInfo: {
+        hasPreviousPage: currentPage > 1,
+        hasNextPage: currentPage < totalPages,
+        currentPage,
+        totalPages,
+        totalItems,
       },
-    })
+    }
+
+    await setCache(KEY, results)
   }
 
-  const paymentTerms = await PaymentTermRepository.paginateByFilters(
-    (currentPage - 1) * itemsPerPage,
-    itemsPerPage,
-    where
-  )
-
-  const totalItems = await PaymentTermRepository.countAllByFilters(where)
-  const totalPages = Math.ceil(totalItems / itemsPerPage)
-
-  return {
-    data: paymentTerms,
-    pageInfo: {
-      hasPreviousPage: currentPage > 1,
-      hasNextPage: currentPage < totalPages,
-      currentPage,
-      totalPages,
-      totalItems,
-    },
-  }
+  return results
 }
