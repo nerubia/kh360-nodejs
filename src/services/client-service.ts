a55f501837d4939974afad6c9a2f920a7d1a704d
jatnerubia <jasonlerit077@gmail.com>
2024-09-26 10:33:15 +0800

    feat: redis

--- src/services/client-service.ts
+++ src/services/client-service.ts
@@ -1,7 +1,17 @@
 import * as ClientRepository from "../repositories/client-repository"
+import { getCache, setCache } from "../utils/redis"
+
+const KEY = "CLIENTS"
 
 export const getActiveClients = async () => {
-  return await ClientRepository.getAllByFilters({
-    status: "Active",
-  })
+  let results = await getCache(KEY)
+
+  if (results === null) {
+    results = await ClientRepository.getAllByFilters({
+      status: "Active",
+    })
+    await setCache(KEY, results)
+  }
+
+  return results
 }
