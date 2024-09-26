1d82287b7f0935f89a25199ee8cfa39398fc6013
jatnerubia <jasonlerit077@gmail.com>
2024-09-26 15:42:46 +0800

    update: country, currency, payment account, term, tax type and oum services

--- src/services/khbooks/currency-service.ts
+++ src/services/khbooks/currency-service.ts
@@ -1,30 +1,41 @@
 import { type Prisma } from "@prisma/client"
 import * as CurrencyRepository from "../../repositories/khbooks/currency-repository"
+import { getCache, setCache } from "../../utils/redis"
 
 export const getAllByFilters = async (page: string) => {
-  const itemsPerPage = 20
-  const parsedPage = parseInt(page)
-  const currentPage = isNaN(parsedPage) || parsedPage < 0 ? 1 : parsedPage
+  const KEY = `CURRENCIES_${page}`
 
-  const where: Prisma.currenciesWhereInput = {}
+  let results = await getCache(KEY)
 
-  const currencies = await CurrencyRepository.paginateByFilters(
-    (currentPage - 1) * itemsPerPage,
-    itemsPerPage,
-    where
-  )
+  if (results === null) {
+    const itemsPerPage = 20
+    const parsedPage = parseInt(page)
+    const currentPage = isNaN(parsedPage) || parsedPage < 0 ? 1 : parsedPage
 
-  const totalItems = await CurrencyRepository.countAllByFilters(where)
-  const totalPages = Math.ceil(totalItems / itemsPerPage)
+    const where: Prisma.currenciesWhereInput = {}
 
-  return {
-    data: currencies,
-    pageInfo: {
-      hasPreviousPage: currentPage > 1,
-      hasNextPage: currentPage < totalPages,
-      currentPage,
-      totalPages,
-      totalItems,
-    },
+    const currencies = await CurrencyRepository.paginateByFilters(
+      (currentPage - 1) * itemsPerPage,
+      itemsPerPage,
+      where
+    )
+
+    const totalItems = await CurrencyRepository.countAllByFilters(where)
+    const totalPages = Math.ceil(totalItems / itemsPerPage)
+
+    results = {
+      data: currencies,
+      pageInfo: {
+        hasPreviousPage: currentPage > 1,
+        hasNextPage: currentPage < totalPages,
+        currentPage,
+        totalPages,
+        totalItems,
+      },
+    }
+
+    await setCache(KEY, results)
   }
+
+  return results
 }
