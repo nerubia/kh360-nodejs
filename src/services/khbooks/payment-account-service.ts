b87ab8abecd20f4f32b84e5384dad6c27d895b3d
jatnerubia <jasonlerit077@gmail.com>
2024-09-26 15:42:46 +0800

    update: country, currency, payment account, term, tax type and oum services

--- src/services/khbooks/payment-account-service.ts
+++ src/services/khbooks/payment-account-service.ts
@@ -1,30 +1,41 @@
 import { type Prisma } from "@prisma/client"
 import * as PaymentAccountRepository from "../../repositories/khbooks/payment-account-repository"
+import { getCache, setCache } from "../../utils/redis"
 
 export const getAllByFilters = async (page: string) => {
-  const itemsPerPage = 20
-  const parsedPage = parseInt(page)
-  const currentPage = isNaN(parsedPage) || parsedPage < 0 ? 1 : parsedPage
+  const KEY = `PAYMENT_ACCOUNTS_${page}`
 
-  const where: Prisma.payment_accountsWhereInput = {}
+  let results = await getCache(KEY)
 
-  const paymentAccounts = await PaymentAccountRepository.paginateByFilters(
-    (currentPage - 1) * itemsPerPage,
-    itemsPerPage,
-    where
-  )
+  if (results === null) {
+    const itemsPerPage = 20
+    const parsedPage = parseInt(page)
+    const currentPage = isNaN(parsedPage) || parsedPage < 0 ? 1 : parsedPage
 
-  const totalItems = await PaymentAccountRepository.countAllByFilters(where)
-  const totalPages = Math.ceil(totalItems / itemsPerPage)
+    const where: Prisma.payment_accountsWhereInput = {}
 
-  return {
-    data: paymentAccounts,
-    pageInfo: {
-      hasPreviousPage: currentPage > 1,
-      hasNextPage: currentPage < totalPages,
-      currentPage,
-      totalPages,
-      totalItems,
-    },
+    const paymentAccounts = await PaymentAccountRepository.paginateByFilters(
+      (currentPage - 1) * itemsPerPage,
+      itemsPerPage,
+      where
+    )
+
+    const totalItems = await PaymentAccountRepository.countAllByFilters(where)
+    const totalPages = Math.ceil(totalItems / itemsPerPage)
+
+    results = {
+      data: paymentAccounts,
+      pageInfo: {
+        hasPreviousPage: currentPage > 1,
+        hasNextPage: currentPage < totalPages,
+        currentPage,
+        totalPages,
+        totalItems,
+      },
+    }
+
+    await setCache(KEY, results)
   }
+
+  return results
 }
--- src/services/khbooks/tax-type-service.ts
+++ src/services/khbooks/tax-type-service.ts
@@ -1,38 +1,49 @@
 import { type Prisma } from "@prisma/client"
 import * as TaxTypeRepository from "../../repositories/khbooks/tax-type-repository"
+import { getCache, setCache } from "../../utils/redis"
 
 export const getAllByFilters = async (name: string, page: string) => {
-  const itemsPerPage = 20
-  const parsedPage = parseInt(page)
-  const currentPage = isNaN(parsedPage) || parsedPage < 0 ? 1 : parsedPage
+  const KEY = `TAX_TYPES_${name}_${page}`
 
-  const where: Prisma.tax_typesWhereInput = {}
+  let results = await getCache(KEY)
 
-  if (name !== undefined) {
-    Object.assign(where, {
-      name: {
-        contains: name,
+  if (results === null) {
+    const itemsPerPage = 20
+    const parsedPage = parseInt(page)
+    const currentPage = isNaN(parsedPage) || parsedPage < 0 ? 1 : parsedPage
+
+    const where: Prisma.tax_typesWhereInput = {}
+
+    if (name !== undefined) {
+      Object.assign(where, {
+        name: {
+          contains: name,
+        },
+      })
+    }
+
+    const taxTypes = await TaxTypeRepository.paginateByFilters(
+      (currentPage - 1) * itemsPerPage,
+      itemsPerPage,
+      where
+    )
+
+    const totalItems = await TaxTypeRepository.countAllByFilters(where)
+    const totalPages = Math.ceil(totalItems / itemsPerPage)
+
+    results = {
+      data: taxTypes,
+      pageInfo: {
+        hasPreviousPage: currentPage > 1,
+        hasNextPage: currentPage < totalPages,
+        currentPage,
+        totalPages,
+        totalItems,
       },
-    })
+    }
+
+    await setCache(KEY, results)
   }
 
-  const taxTypes = await TaxTypeRepository.paginateByFilters(
-    (currentPage - 1) * itemsPerPage,
-    itemsPerPage,
-    where
-  )
-
-  const totalItems = await TaxTypeRepository.countAllByFilters(where)
-  const totalPages = Math.ceil(totalItems / itemsPerPage)
-
-  return {
-    data: taxTypes,
-    pageInfo: {
-      hasPreviousPage: currentPage > 1,
-      hasNextPage: currentPage < totalPages,
-      currentPage,
-      totalPages,
-      totalItems,
-    },
-  }
+  return results
 }
