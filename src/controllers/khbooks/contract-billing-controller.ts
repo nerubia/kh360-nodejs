165d13b29e055923f825ccf0302dc91025a743c0
jatnerubia <jasonlerit077@gmail.com>
2024-10-07 16:45:37 +0800

    update: contract billing api

--- src/controllers/khbooks/contract-billing-controller.ts
+++ src/controllers/khbooks/contract-billing-controller.ts
@@ -11,12 +11,22 @@ import * as ContractBillingService from "../../services/khbooks/contract-billing
  * @param req.query.project_id - Filter by project_id.
  * @param req.query.description - Filter by description.
  * @param req.query.status - Filter by status.
+ * @param req.query.active_contract - Filter by active contract.
  * @param req.query.page - Page number for pagination.
  */
 export const index = async (req: Request, res: Response) => {
   try {
-    const { client_id, start_date, end_date, contract_no, project_id, description, status, page } =
-      req.query
+    const {
+      client_id,
+      start_date,
+      end_date,
+      contract_no,
+      project_id,
+      description,
+      status,
+      active_contract,
+      page,
+    } = req.query
     const results = await ContractBillingService.getAllByFilters(
       parseInt(client_id as string),
       start_date as string,
@@ -25,6 +35,7 @@ export const index = async (req: Request, res: Response) => {
       parseInt(project_id as string),
       description as string,
       status as string,
+      Boolean(active_contract),
       page as string
     )
     res.json(results)
