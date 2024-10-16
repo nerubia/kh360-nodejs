d75c66d957273644ed1c45e27f0b2efbf22aab32
jatnerubia <jasonlerit077@gmail.com>
2024-10-16 15:22:16 +0800

    feat: invoice activity

--- src/repositories/khbooks/invoice-activity-repository.ts
+++ src/repositories/khbooks/invoice-activity-repository.ts
@@ -1,6 +1,30 @@
 import prisma from "../../utils/prisma"
 import { type Prisma } from "@prisma/client"
 
+export const paginateByFilters = async (
+  skip: number,
+  take: number,
+  where: Prisma.invoice_activitiesWhereInput
+) => {
+  return await prisma.invoice_activities.findMany({
+    skip,
+    take,
+    where,
+    orderBy: [
+      {
+        created_at: "desc",
+      },
+    ],
+  })
+}
+
+export const countAllByFilters = async (where: Prisma.invoice_activitiesWhereInput) => {
+  const count = await prisma.invoice_activities.count({
+    where,
+  })
+  return count
+}
+
 export const create = async (data: Prisma.invoice_activitiesUncheckedCreateInput) => {
   return await prisma.invoice_activities.create({
     data,
