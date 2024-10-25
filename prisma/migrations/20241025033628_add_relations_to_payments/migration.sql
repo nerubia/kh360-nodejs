f4004f4fc849239902ae2bd461c1b0acd9bedef8
emmaacha <emmalynacha@gmail.com>
2024-10-25 11:27:26 +0800

    add relations for payments and payment details

---
+++ prisma/migrations/20241025033628_add_relations_to_payments/migration.sql
@@ -0,0 +1,20 @@
+-- CreateIndex
+CREATE INDEX `index_payment_details_on_payment_id` ON `payment_details`(`payment_id`);
+
+-- AddForeignKey
+ALTER TABLE `payment_attachments` ADD CONSTRAINT `payment_attachments_payment_id_fkey` FOREIGN KEY (`payment_id`) REFERENCES `payments`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `payment_details` ADD CONSTRAINT `payment_details_payment_id_fkey` FOREIGN KEY (`payment_id`) REFERENCES `payments`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `payment_details` ADD CONSTRAINT `payment_details_invoice_id_fkey` FOREIGN KEY (`invoice_id`) REFERENCES `invoices`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `payment_emails` ADD CONSTRAINT `payment_emails_payment_id_fkey` FOREIGN KEY (`payment_id`) REFERENCES `payments`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `payments` ADD CONSTRAINT `payments_company_id_fkey` FOREIGN KEY (`company_id`) REFERENCES `companies`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `payments` ADD CONSTRAINT `payments_currency_id_fkey` FOREIGN KEY (`currency_id`) REFERENCES `currencies`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
