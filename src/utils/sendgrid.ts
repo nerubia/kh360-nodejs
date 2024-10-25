3fde3f51eb89b24c03bac0357406a956995b94cd
jatnerubia <jasonlerit077@gmail.com>
2024-10-25 08:25:45 +0800

    update: remove duplicate emails when sending emails

--- src/utils/sendgrid.ts
+++ src/utils/sendgrid.ts
@@ -1,4 +1,5 @@
 import sgMail from "@sendgrid/mail"
+import CustomError from "./custom-error"
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY as string)
 
@@ -18,20 +19,42 @@ interface MailProps {
   attachments?: Attachment[]
 }
 
+const getUniqueEmails = (group: string[], others: string[]) => {
+  const uniqueEmails: string[] = []
+  for (const email of group) {
+    if (!uniqueEmails.includes(email)) {
+      uniqueEmails.push(email)
+    }
+  }
+  return uniqueEmails.filter((email) => !others.includes(email))
+}
+
 export const sendMail = async ({ to, cc, bcc, subject, content, attachments }: MailProps) => {
   try {
+    const uniqueTo: string[] = getUniqueEmails(to, [])
+    let uniqueCc: string[] = []
+    let uniqueBcc: string[] = []
+
+    if (cc !== undefined) {
+      uniqueCc = getUniqueEmails(cc, uniqueTo)
+    }
+    if (bcc !== undefined) {
+      uniqueBcc = getUniqueEmails(bcc, [...uniqueTo, ...uniqueCc])
+    }
+
     const msg = {
-      to,
-      cc,
-      bcc,
+      to: uniqueTo,
+      cc: uniqueCc,
+      bcc: uniqueBcc,
       from: process.env.SENDGRID_FROM_ADDRESS as string,
       subject,
       html: content,
       attachments,
     }
+
     return await sgMail.send(msg)
   } catch (error) {
-    return null
+    throw new CustomError("Unable to send email", 400)
   }
 }
 
