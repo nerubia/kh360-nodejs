5702d4a87db314a297344d636f90a4ea3bce6b4f
jatnerubia <jasonlerit077@gmail.com>
2024-10-25 13:06:11 +0800

    update: change payment reference number to payment number

--- src/controllers/khbooks/payment-controller.ts
+++ src/controllers/khbooks/payment-controller.ts
@@ -7,18 +7,18 @@ import * as PaymentService from "../../services/khbooks/payment-service"
  * @param req.query.payment_date - Filter by payment date.
  * @param req.query.client_id - Filter by client_id.
  * @param req.query.invoice_no - Filter by invoice no.
- * @param req.query.payment_reference_no - Filter by payment reference no.
+ * @param req.query.payment_no - Filter by payment no.
  * @param req.query.status - Filter by status.
  * @param req.query.page - Page number for pagination.
  */
 export const index = async (req: Request, res: Response) => {
   try {
-    const { payment_date, client_id, invoice_no, payment_reference_no, status, page } = req.query
+    const { payment_date, client_id, invoice_no, payment_no, status, page } = req.query
     const results = await PaymentService.getAllByFilters(
       payment_date as string,
       parseInt(client_id as string),
       invoice_no as string,
-      payment_reference_no as string,
+      payment_no as string,
       status as string,
       page as string
     )
