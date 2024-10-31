2aebdc69de1ce4a0c1a3c23e1f1d0d39d24f42eb
jatnerubia <jasonlerit077@gmail.com>
2024-10-31 14:55:50 +0800

    fix: payment network filter

--- src/types/payment-account-type.ts
+++ src/types/payment-account-type.ts
@@ -20,10 +20,10 @@ export interface PaymentAccount {
 }
 
 export interface PaymentAccountFilters {
-  payment_account_name?: string
-  payment_network?: string
-  account_name?: string
-  account_no?: string
-  bank_name?: string
+  payment_account_name: string
+  payment_network_id: number
+  account_name: string
+  account_no: string
+  bank_name: string
   page?: string
 }
