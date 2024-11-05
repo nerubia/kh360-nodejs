6744d499486daa1d610648d5e638bc5ae924ca7b
jatnerubia <jasonlerit077@gmail.com>
2024-11-05 14:56:04 +0800

    update: add system_name column to email templates

--- src/utils/validation/email-template-schema.ts
+++ src/utils/validation/email-template-schema.ts
@@ -9,4 +9,5 @@ export const addEmailTemplate = object().shape({
     .max(100, "Template type should not exceed 100 characters."),
   subject: string().max(255, "Subject should not exceed 255 characters."),
   content: string().required("Content is required"),
+  system_name: string().max(100, "System name should not exceed 255 characters."),
 })
