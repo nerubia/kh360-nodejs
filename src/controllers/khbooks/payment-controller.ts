6960ced4b8f1f9bc3f74f2d11fb35959dd6176dc
jatnerubia <jasonlerit077@gmail.com>
2024-11-19 15:21:23 +0800

    fix: cancel payment message

--- src/controllers/khbooks/payment-controller.ts
+++ src/controllers/khbooks/payment-controller.ts
@@ -243,7 +243,7 @@ export const cancel = async (req: Request, res: Response) => {
   try {
     const { id } = req.params
     await PaymentService.cancel(parseInt(id))
-    res.json({ message: "Invoice has been cancelled" })
+    res.json({ message: "Payment has been cancelled" })
   } catch (error) {
     if (error instanceof CustomError) {
       return res.status(error.status).json({ message: error.message })
