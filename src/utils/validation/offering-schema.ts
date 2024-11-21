2c727671be8dbbb2c827f4f04896e76eab2fd3c2
jatnerubia <jasonlerit077@gmail.com>
2024-11-21 08:43:39 +0800

    update: add "orderBy" to list offering api

--- src/utils/validation/offering-schema.ts
+++ src/utils/validation/offering-schema.ts
@@ -1,4 +1,4 @@
-import { boolean, number, object, string } from "yup"
+import { array, boolean, number, object, string } from "yup"
 
 export const createOfferingSchema = object().shape({
   name: string().required("Name is required"),
@@ -8,4 +8,29 @@ export const createOfferingSchema = object().shape({
   price: number().required("Price is required"),
   description: string().optional(),
   is_active: boolean().required(),
+})
+
+export const offeringOrderBySchema = array().of(
+  object()
+    .shape({
+      name: string().oneOf(["asc", "desc"]).optional(),
+      created_at: string().oneOf(["asc", "desc"]).optional(),
+    })
+    .test(
+      "only-one-field",
+      'Object must contain either "name" or "created_at", but not both',
+      function (value) {
+        const hasName = value?.name !== undefined
+        const hasCreatedAt = value?.created_at !== undefined
+        return (hasName && !hasCreatedAt) || (!hasName && hasCreatedAt)
+          ? true
+          : this.createError({
+              message: 'Object must contain either "name" or "created_at", but not both',
+            })
+      }
+    )
+)
+
+export const offeringSchema = object().shape({
+  orderBy: offeringOrderBySchema,
 })
