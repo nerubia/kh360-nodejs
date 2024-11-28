9f71d75f5079c9e71b3c1bad8cbe6c6f6e9c41f2
jatnerubia <jasonlerit077@gmail.com>
2024-11-28 15:25:28 +0800

    fix: project assignment filter

--- src/services/project-member-service.ts
+++ src/services/project-member-service.ts
@@ -144,38 +144,40 @@ export const getAllByFilters = async (
   }
 
   if (overlap) {
-    Object.assign(filter, {
-      OR: [
-        {
-          start_date: {
-            gte: startDate,
-            lte: endDate,
+    if (startDate !== undefined || endDate !== undefined) {
+      Object.assign(filter, {
+        OR: [
+          {
+            start_date: {
+              gte: startDate,
+              lte: endDate,
+            },
           },
-        },
-        {
-          end_date: {
-            gte: startDate,
-            lte: endDate,
+          {
+            end_date: {
+              gte: startDate,
+              lte: endDate,
+            },
           },
-        },
-        {
-          start_date: {
-            lte: startDate,
+          {
+            start_date: {
+              lte: startDate,
+            },
+            end_date: {
+              gte: startDate,
+            },
           },
-          end_date: {
-            gte: startDate,
+          {
+            start_date: {
+              lte: endDate,
+            },
+            end_date: {
+              gte: endDate,
+            },
           },
-        },
-        {
-          start_date: {
-            lte: endDate,
-          },
-          end_date: {
-            gte: endDate,
-          },
-        },
-      ],
-    })
+        ],
+      })
+    }
   } else {
     if (startDate !== undefined || endDate !== undefined) {
       Object.assign(filter, {
