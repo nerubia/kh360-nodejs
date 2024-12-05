a267ce4a65c435c999273764ae1194027dd23121
jatnerubia <jasonlerit077@gmail.com>
2024-12-05 11:01:06 +0800

    feat: capture open email

--- src/controllers/webhook-controller.ts
+++ src/controllers/webhook-controller.ts
@@ -2,6 +2,7 @@ import { type Request, type Response } from "express"
 import { verifySendGridRequest } from "../utils/sendgrid"
 import * as InvoiceActivityService from "../services/khbooks/invoice-activity-service"
 import * as InvoiceService from "../services/khbooks/invoice-service"
+import * as EmailLogRepository from "../repositories/email-log-repository"
 import { InvoiceActivityAction } from "../types/invoice-activity-type"
 import { InvoiceStatus } from "../types/invoice-type"
 import { type SendGridEvent, SendGridEventType } from "../types/sendgrid-type"
@@ -17,22 +18,36 @@ export const handleSendGrid = async (req: Request, res: Response) => {
     const events: SendGridEvent[] = parsedBody
 
     for (const event of events) {
+      const mailId = event.sg_message_id.split(".")[0]
+
+      const emailLog = await EmailLogRepository.getByMailId(mailId)
+
+      if (emailLog === null) continue
+
+      const notes = JSON.parse(emailLog.notes ?? "")
+      const invoiceId = notes.invoice_id as number
+
+      const invoice = await InvoiceService.getInvoiceById(invoiceId)
+
+      if (invoice === null) continue
+
+      if (event.event === SendGridEventType.OPEN) {
+        await InvoiceActivityService.create({
+          action: InvoiceActivityAction.OPENED,
+          description: event.email,
+          invoice_id: invoice.id,
+        })
+      }
+
       if (event.event === SendGridEventType.CLICK) {
-        const splitUrl = event.url.split("/")
-        const token = splitUrl.at(-1) ?? ""
+        await InvoiceActivityService.create({
+          action: InvoiceActivityAction.VIEWED,
+          description: event.email,
+          invoice_id: invoice.id,
+        })
 
-        const invoice = await InvoiceService.getInvoiceByToken(token)
-
-        if (invoice !== null) {
-          await InvoiceActivityService.create({
-            action: InvoiceActivityAction.VIEWED,
-            description: event.email,
-            invoice_id: invoice.id,
-          })
-
-          if (invoice.invoice_status === InvoiceStatus.BILLED) {
-            await InvoiceService.updateInvoiceStatusById(invoice.id, InvoiceStatus.VIEWED)
-          }
+        if (invoice.invoice_status === InvoiceStatus.BILLED) {
+          await InvoiceService.updateInvoiceStatusById(invoice.id, InvoiceStatus.VIEWED)
         }
       }
     }
